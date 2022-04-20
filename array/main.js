16a83b9e173dbeade82b584c2f7e24f28b4dbe39
oswi <oswaldo.adrian@live.com.mx>
2022-04-20 15:53:12 -0500

    init array

---
+++ array/main.js
@@ -0,0 +1,24 @@
+class MyArray {
+  
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+}
+
+const myArray = new MyArray();
+myArray.push('hello');
+myArray.push('world');
+
+console.log(myArray.get(1));
