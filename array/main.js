4baf608f041f698d764ba2d711d1f6e3802347da
oswi <oswaldo.adrian@live.com.mx>
2022-04-20 23:06:34 -0500

    pop & delete

--- array/main.js
+++ array/main.js
@@ -14,11 +14,39 @@ class MyArray {
     this.length++;
     return this.length;
   }
+  
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    const item = this.data[index];
+    this.shiftIndex(index);
+    return item;
+  }
+
+  shiftIndex(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+      console.log(this);
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+
+  //  TODO: agregar y borrar elemento al inicio
 
 }
 
 const myArray = new MyArray();
-myArray.push('hello');
-myArray.push('world');
-
-console.log(myArray.get(1));
+myArray.push(1);
+myArray.push(2);
+myArray.push(3);
+myArray.push(4);
+myArray.push(5);
+console.log(myArray);
+console.log(myArray.delete(2));
+console.log(myArray);
